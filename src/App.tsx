02c984c68092b3ab674932f073bc233e7863d164
Alex Santos <devalexsantos@gmail.com>
2024-01-29 12:05:26 -0300

    feat: aditioned resume layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Header } from "./components/layout/header"
+import { Resume } from "./components/resume"
 
 export function App() {
   return(
-    <div className="max-w-4xl m-auto">
+    <div className="max-w-4xl m-auto flex flex-col gap-4">
     <Header />
+    <Resume />
     <div className="container p-8">
       <h1>Hellow</h1>
     </div>
---
+++ src/components/resume.tsx
@@ -0,0 +1,24 @@
+export function Resume(){
+    return(
+        <div className="container p-8 flex gap-4 flex-wrap">
+            <div className="flex flex-1 h-[150px] flex-col gap-3 items-center justify-center rounded border">
+                <span className="text-base text-muted-foreground">
+                    Tarefas a fazer:
+                </span>
+                <span className="text-5xl font-bold">
+                    03
+                </span>
+            </div>
+
+            <div className="flex flex-1 h-[150px] flex-col gap-3 items-center justify-center rounded border">
+                <span className="text-base text-muted-foreground">
+                    Tempo total:
+                </span>
+                <div className="flex items-end gap-2">
+                    <span className="text-5xl font-bold">02</span>
+                    <span className="text-xl">h</span>
+                </div>
+            </div>
+        </div>
+    )
+}
