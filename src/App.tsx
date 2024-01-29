aecab8a11fe5df2fe4d35d28e5a2f3478685c9f9
Alex Santos <devalexsantos@gmail.com>
2024-01-29 11:06:01 -0300

    first commit: setup shadcn and tailwind

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+export function App() {
+  return(
+    <>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <Button>Botao</Button>
+    </>
+  )
+}
