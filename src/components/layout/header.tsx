4932538b705f8fe490e66136793f5c964c6cfb7c
Alex Santos <devalexsantos@gmail.com>
2024-01-29 12:07:28 -0300

    chore: adjusting logo

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -3,7 +3,7 @@ import { ModeToggle } from "../mode-toggle";
 export function Header() {
     return(
         <div className="container p-8 flex justify-between">
-            <span className="text-xl font-bold italic">fastodo.</span>
+            <span className="text-xl font-bold italic">fastodo</span>
             <ModeToggle />
         </div>
     )
